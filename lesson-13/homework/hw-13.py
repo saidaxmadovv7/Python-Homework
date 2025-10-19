b4b91169d05bc956ecc3e02824b14d34628410cb
Saidamin Saidakhmadov <saidaxmadovsaidamin@gmail.com>
2025-10-19 22:39:34 +0500

    hw-13.py

---
+++ lesson-13/homework/hw-13.py
@@ -0,0 +1,113 @@
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
+
+
+
+
+#2
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
+
+
+#3
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
+
+
+#4
+
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
+
+
+#5
+
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
+
+
+
+#6
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
+
+
+
+#7
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
+
+#8
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
+
+
+#9
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
+
+
+#10
+
+import re
+
+text = input("Matn kiriting: ")
+pattern = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{2}/\d{2}/\d{4}\b'
+
+dates = re.findall(pattern, text)
+print("Topilgan sanalar:", dates if dates else "Yo‘q")
+
