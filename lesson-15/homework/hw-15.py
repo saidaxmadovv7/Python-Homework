b01fd0f6271ce3b5dd36de5b95a25bdfcd8a7571
Saidamin Saidakhmadov <saidaxmadovsaidamin@gmail.com>
2025-10-28 20:24:17 +0500

    Update hw-15.py

--- lesson-15/homework/hw-15.py
+++ lesson-15/homework/hw-15.py
@@ -1,188 +1,68 @@
-#1. Convert List to 1D Array
-#Write a NumPy program to convert a list of numeric values into a one-dimensional NumPy array.
+#---------------------------------------------------
+#---------------------------------------------------
+# Create a new database with a table named Roster that has three fields: Name, Species, and Age. The Name and Species columns should be text fields, and the Age column should be an integer field.
 
-#Expected Output:
+import sqlite3
 
-#Original List: [12.23, 13.32, 100, 36.32] One-dimensional NumPy array: [ 12.23 13.32 100. 36.32]
+with sqlite3.connect("example.db") as conn:
+    cursor = conn.cursor()
+    
+    # Jadvalni yaratish (agar hali yaratilmagan bo‘lsa)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Roster (
+        Name TEXT,
+        Species TEXT,
+        Age INTEGER
+    )
+    """)
+    
+    # Ma'lumotlarni kiritish
+    data = [
+        ("Benjamin Sisko", "Human", 40),
+        ("Jadzia Dax", "Trill", 300),
+        ("Kira Nerys", "Bajoran", 29)
+    ]
+    
+    cursor.executemany("INSERT INTO Roster (Name, Species, Age) VALUES (?, ?, ?)", data)
+    conn.commit()
 
-import numpy as np
 
-arr = np.array([12.23, 13.32, 100, 36.32])
-print(arr)
 
 
 
+import sqlite3
 
-#2. Create 3x3 Matrix (2?10)
-#Write a NumPy program to create a 3x3 matrix with values ranging from 2 to 10.
+with sqlite3.connect("example.db") as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Roster")
+    rows = cursor.fetchall()    # Barcha natijalarni olish uchun ishlatilinadi
 
-#Expected Output:
+    for i in rows:
+        print(rows)
 
-#[[ 2 3 4] [ 5 6 7] [ 8 9 10]]
 
-import numpy as np
 
-matrix = np.array(([2,3,4],[5,6,7],[8,9,10]))
-print(matrix)
 
-print (matrix.shape)
+# Update the Name of Jadzia Dax to be Ezri Dax
 
+import sqlite3
 
+with sqlite3.connect("example.db") as conn:
+    cursor = conn.cursor()
+    cursor.execute("UPDATE Roster SET name =? WHERE name=?",("Jadzia Dax","Ezri Dax"))
 
 
-#3. Null Vector (10) & Update Sixth Value
-#Write a NumPy program to create a null vector of size 10 and update the sixth value to 11.
 
-#[ 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
 
-#Update sixth value to 11 [ 0. 0. 0. 0. 0. 0. 11. 0. 0. 0.]
+# Display the Name and Age of everyone in the table classified as Bajoran.
 
-import numpy as np
+with sqlite3.connect("example.db") as conn:
+    cursor.execute("SELECT Name, Age FROM Roster WHERE Species = 'Bajoran'")
+    print(cursor.fetchall())
 
-arr = np.zeros(10)
 
-print("Original array:")
-print(arr)
 
-arr[5] = 11
 
-print("\nUpdated array:")
-print(arr)
-
-
-
-
-#4. Array from 12 to 38
-#Write a NumPy program to create an array with values ranging from 12 to 38.
-
-#Expected Output:
-
-#[12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37]
-
-import numpy as np
-arr = np.arange(12, 38)
-print(arr)
-
-
-
-
-#5. Convert Array to Float Type
-#Write a NumPy program to convert an array to a floating type.
-
-#Sample output:
-
-#Original array [1, 2, 3, 4]
-
-import numpy as np
-
-arr = np.array([1, 2, 3, 4])
-print("Original array:", arr)
-
-float_arr = arr.astype(float)
-print("Array converted to float:", float_arr)
-
-
-
-
-
-#6. Celsius to Fahrenheit Conversion
-#Write a NumPy program to convert Centigrade degrees into Fahrenheit degrees. Centigrade values are stored in a NumPy array.
-
-#Sample Array [0, 12, 45.21, 34, 99.91] [-17.78, -11.11, 7.34, 1.11, 37.73, 0. ]
-
-#Expected Output:
-
-#Values in Fahrenheit degrees: [ 0. 12. 45.21 34. 99.91 32. ]
-
-#Values in Centigrade degrees: [-17.78 -11.11 7.34 1.11 37.73 0. ]
-
-#Values in Centigrade degrees: [-17.78 -11.11 7.34 1.11 37.73 0. ]
-
-#Values in Fahrenheit degrees: [-0. 12. 45.21 34. 99.91 32. ]
-
-import numpy as np
-
-c_values = np.array([0, 12, 45.21, 34, 99.91])
-
-# Selsiy → Farangeyt formulasi: F = (9/5) * C + 32
-f_values = (9/5) * c_values + 32
-
-print("Values in Centigrade degrees:", c_values)
-print("Values in Fahrenheit degrees:", f_values)
-
-
-
-
-
-#7. Append Values to Array (Do self-tudy)
-# Write a NumPy program to append values to the end of an array.
-
-#Expected Output:
-
-#Original array: [10, 20, 30]
-
-#After append values to the end of the array: [10 20 30 40 50 60 70 80 90]
-
-import numpy as np
-
-arr = np.array([10, 20, 30])
-print("Original array:", arr)
-
-new_arr = np.append(arr, [40, 50, 60, 70, 80, 90])
-
-print("After append values to the end of the array:", new_arr)
-
-
-
-
-
-#8. Array Statistical Functions (Do self-tudy)
-# Create a random NumPy array of 10 elements and calculate the mean, median, and standard deviation of the array.
-
-import numpy as np
-
-arr = np.random.rand(10)
-
-print("Random array:", arr)
-
-mean_value = np.mean(arr)
-
-median_value = np.median(arr)
-
-std_value = np.std(arr)
-
-print("Mean:", mean_value)
-print("Median:", median_value)
-print("Standard Deviation:", std_value)
-
-
-
-
-
-#9 Find min and max
-#Create a 10x10 array with random values and find the minimum and maximum values.
-
-import numpy as np
-
-arr = np.random.rand(10, 10)
-
-print("10x10 random array:\n", arr)
-
-min_value = np.min(arr)
-max_value = np.max(arr)
-
-print("\nMinimum value:", min_value)
-print("Maximum value:", max_value)
-
-
-
-
-
-#10
-#Create a 3x3x3 array with random values.
-
-import numpy as np
-
-arr = np.random.rand(3, 3, 3)
-
-print("3x3x3 random array:\n", arr)
+with sqlite3.connect("company1.db") as conn:
+    cursor.execute ("SELECT*FROM Roster")
+    print(cursor.fetchall())
